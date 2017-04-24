48221b79b6bf1824b104cb0b4654b6b4529d8dc1
Jong Ha Lee <jonghalee@berkeley.edu>
2017-04-23 22:20:35 -0700

    relative paths on vaibhav

--- code/vaibhav1.R
+++ code/vaibhav1.R
@@ -1,11 +1,15 @@
 library(data.table)
 library(dplyr)
 
-business <- fread("/Users/vaibhav/Documents/Year3_Junior/Semester 2/Stat 154/project/data/yelp_academic_dataset_business_train.csv", data.table = FALSE)
-checkin <- fread("/Users/vaibhav/Documents/Year3_Junior/Semester 2/Stat 154/project/data/yelp_academic_dataset_checkin.csv", data.table = FALSE)
-review <- fread("/Users/vaibhav/Documents/Year3_Junior/Semester 2/Stat 154/project/data/yelp_academic_dataset_review_train.csv", data.table = FALSE)
-tip <- fread("/Users/vaibhav/Documents/Year3_Junior/Semester 2/Stat 154/project/data/yelp_academic_dataset_tip.csv", data.table = FALSE)
-user <- fread("/Users/vaibhav/Documents/Year3_Junior/Semester 2/Stat 154/project/data/yelp_academic_dataset_user.csv", data.table = FALSE)
+setwd("../")
+# business <- fread("data/train/yelp_academic_dataset_business_train.csv", 
+#                   data.table = FALSE)
+# checkin <- fread("/Users/vaibhav/Documents/Year3_Junior/Semester 2/Stat 154/project/data/yelp_academic_dataset_checkin.csv", data.table = FALSE)
+# review <- fread("/Users/vaibhav/Documents/Year3_Junior/Semester 2/Stat 154/project/data/yelp_academic_dataset_review_train.csv", data.table = FALSE)
+# tip <- fread("/Users/vaibhav/Documents/Year3_Junior/Semester 2/Stat 154/project/data/yelp_academic_dataset_tip.csv", data.table = FALSE)
+# user <- fread("/Users/vaibhav/Documents/Year3_Junior/Semester 2/Stat 154/project/data/yelp_academic_dataset_user.csv", data.table = FALSE)
+
+load("data/train/alltrain.RData")
 
 ## EDA to explore review+tip join issue
 
