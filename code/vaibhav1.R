7f0aa0384154b9e1e493b3559d3e4e88873d303d
Vaibhav Ramamoorthy <vaibhavr@berkeley.edu>
2017-04-24 15:05:24 -0700

    completed the hours analysis though would love more brainstorming on features here

--- code/vaibhav1.R
+++ code/vaibhav1.R
@@ -32,7 +32,9 @@ t. <- t %>%
 ######################## ONLY VAIBHAV BELOW #############################
 
 array_parser <- function(df, column, fn) {
-  return(lapply(strsplit(gsub("'|[[:space:]]|\\[|\\]","",df[,column]),","), fn))
+  cleaned_col <- gsub("'|[[:space:]]|\\[|\\]","",df[,column])
+  vector <- strsplit(cleaned_col,",")
+  return(lapply(vector, fn))
 }
 
 #### BUSINESS.CATEGORIES
@@ -50,14 +52,65 @@ business.categories_fn <- function(categories) {
 }
 
 business.categories.d <- array_parser(business, "categories", business.categories_fn)
-business.categories <- data.frame(matrix(unlist(q), nrow=length(q), byrow=T))
+business.categories <- data.frame(matrix(unlist(business.categories.d), nrow=length(business.categories.d), byrow=T))
 colnames(business.categories) <- c("Mexican", "Restaurants", "Coffee&Tea", "Food", "Pizza", "Chinese")
 
 #### BUSINESS.HOURS
 
 business.hours_fn <- function(hours) {
+  hours <- paste(hours,"-",sep="")
+  if(hours == "-") {
+    rval <- rep(NA,23)
+    names(rval) <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",
+                     "Mon_ct", "Tues_ct", "Wed_ct", "Thurs_ct", "Fri_ct", "Sat_ct", "Sun_ct",
+                     "Mon_hrs", "Tues_hrs", "Wed_hrs", "Thurs_hrs", "Fri_hrs", "Sat_hrs", "Sun_hrs",
+                     "total_hrs", "avg_hrs")
+    return(rval)
+  }
+  parsed <- unlist(strsplit(hours, "y|-"))
+  parsed.df <- data.frame(matrix(parsed, ncol=3, byrow=T), stringsAsFactors = F)
+  names(parsed.df) <- c("day", "start", "end")
   
+  time_completer <- function(time) {
+    return(as.ITime(ifelse(time=="0:0", "23:59:59", time)))
+  }
+  
+  time_subtracter <- function(start, end) {
+    if(end >= start) {
+      return(round(as.numeric(end - start)/3600))
+    } else {
+      return(round((as.numeric(as.ITime("23:59:59") - start) + as.numeric(end - as.ITime("0:00")))/3600))
+    }
+  }
+  
+  parsed.df <- parsed.df %>%
+    transmute(day = paste(day, "y", sep=""), 
+              start = time_completer(start),
+              end = time_completer(end)) %>%
+    mutate(hours = time_subtracter(start, end))
+  print(parsed.df)
+
+  rvec <- rep(0,23)
+  d <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",
+         "Mon_ct", "Tues_ct", "Wed_ct", "Thurs_ct", "Fri_ct", "Sat_ct", "Sun_ct",
+         "Mon_hrs", "Tues_hrs", "Wed_hrs", "Thurs_hrs", "Fri_hrs", "Sat_hrs", "Sun_hrs",
+         "total_hrs", "avg_hrs")
+  names(rvec) <- d
+  for(i in 1:7) {
+    rvec[i] <- ifelse(sum(d[i] == parsed.df$day) > 0, 1, 0)
+    rvec[i+7] <- sum(d[i] == parsed.df$day)
+    rvec[i+14] <- sum(parsed.df$hours[parsed.df$day == d[i]])
+    rvec['total_hrs'] <- sum(parsed.df$hours)
+    rvec['avg_hrs'] <- mean(parsed.df$hours)
+  }
+  return(rvec)
 }
 
+business.hours.d <- array_parser(business, "hours", business.hours_fn)
+business.hours <- data.frame(matrix(unlist(business.hours.d), nrow=length(business.hours.d), byrow=T))
+colnames(business.hours) <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",
+                              "Mon_ct", "Tues_ct", "Wed_ct", "Thurs_ct", "Fri_ct", "Sat_ct", "Sun_ct",
+                              "Mon_hrs", "Tues_hrs", "Wed_hrs", "Thurs_hrs", "Fri_hrs", "Sat_hrs", "Sun_hrs",
+                              "total_hrs", "avg_hrs")
 
 
