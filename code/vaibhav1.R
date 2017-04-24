13f7966cc2a2da5e8c3d2036a7a93bedce14f33d
Vaibhav Ramamoorthy <vaibhavr@berkeley.edu>
2017-04-23 22:04:55 -0700

    array parser

--- code/vaibhav1.R
+++ code/vaibhav1.R
@@ -1,7 +1,45 @@
 library(data.table)
+library(dplyr)
 
 business <- fread("/Users/vaibhav/Documents/Year3_Junior/Semester 2/Stat 154/project/data/yelp_academic_dataset_business_train.csv", data.table = FALSE)
 checkin <- fread("/Users/vaibhav/Documents/Year3_Junior/Semester 2/Stat 154/project/data/yelp_academic_dataset_checkin.csv", data.table = FALSE)
 review <- fread("/Users/vaibhav/Documents/Year3_Junior/Semester 2/Stat 154/project/data/yelp_academic_dataset_review_train.csv", data.table = FALSE)
 tip <- fread("/Users/vaibhav/Documents/Year3_Junior/Semester 2/Stat 154/project/data/yelp_academic_dataset_tip.csv", data.table = FALSE)
 user <- fread("/Users/vaibhav/Documents/Year3_Junior/Semester 2/Stat 154/project/data/yelp_academic_dataset_user.csv", data.table = FALSE)
+
+## EDA to explore review+tip join issue
+
+r <- review %>%
+  select(business_id, user_id)
+t <- tip %>%
+  select(business_id, user_id)
+j <- left_join(r, t, by = c("business_id" = "business_id", "user_id" = "user_id"))
+
+r. <- r %>%
+  group_by(business_id, user_id) %>%
+  summarize(count = n()) %>%
+  arrange(desc(count))
+t. <- t %>%
+  group_by(business_id, user_id) %>%
+  summarize(count = n()) %>%
+  arrange(desc(count))
+
+array_parser <- function(df, column, fn) {
+  return(sapply(strsplit(gsub("'|[[:space:]]|\\[|\\]","",df[,column]),","), fn))
+}
+
+## ex: array_parser(business, "categories", length)
+
+business.categories_one.hot <- function(categories) {
+  v <- rep(0, 6)
+  n <- c("Mexican", "Restaurants", "Coffee&Tea", "Food", "Pizza", "Chinese")
+  names(v) <- n
+  for(i in 1:length(n)) {
+    if(n[i] %in% categories) {
+      v[i] <- 1
+    }
+  }
+}
+
+array_parser(business, "categories", business.categories_one.hot)
+
